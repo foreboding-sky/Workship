b40a706946d6093f60bf5608818e4dd9bb33fc5d
KARMA\Andriy <andriy.kondratuik@gmail.com>
2024-06-24 19:15:51 +0300

    data seeding

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -32,12 +32,15 @@ namespace Workshop.Data
             if (!Clients.Any())
             {
                 clients.AddRange(new List<Client> {
-                    new Client { Id = Guid.NewGuid(), FullName = "John Doe", Phone = "380991234567" },
-                    new Client { Id = Guid.NewGuid(), FullName = "Andrew Duh", Phone = "380996667890" },
-                    new Client { Id = Guid.NewGuid(), FullName = "Valeria Chan", Phone = "380993434345" },
-                    new Client { Id = Guid.NewGuid(), FullName = "Barak Obama", Phone = "380998769034" },
+                    new Client { Id = Guid.NewGuid(), FullName = "John Smith", Phone = "380991234567" },
+                    new Client { Id = Guid.NewGuid(), FullName = "Andrew Kondratuik", Phone = "380996667890" },
+                    new Client { Id = Guid.NewGuid(), FullName = "Valeria Vozniuk", Phone = "380993434345" },
+                    new Client { Id = Guid.NewGuid(), FullName = "Illya Dobrovolec", Phone = "380998769034" },
                     new Client { Id = Guid.NewGuid(), FullName = "Vitaliy Kim", Phone = "380998976403" },
-                    new Client { Id = Guid.NewGuid(), FullName = "Kaneko Ken", Phone = "380992281337" }
+                    new Client { Id = Guid.NewGuid(), FullName = "Kaneko Ken", Phone = "380992281337" },
+                    new Client { Id = Guid.NewGuid(), FullName = "Ivan Holod", Phone = "380998769034" },
+                    new Client { Id = Guid.NewGuid(), FullName = "Dmutro Maksymiuk", Phone = "380998976403" },
+                    new Client { Id = Guid.NewGuid(), FullName = "Maxim Melnuchyk", Phone = "380992281337" }
                 });
 
                 Clients.AddRange(clients);
@@ -48,8 +51,8 @@ namespace Workshop.Data
             {
                 specialists.AddRange(new List<Specialist>
                 {
-                    new Specialist { Id = Guid.NewGuid(), FullName = "Vanya", Comment = "Comment" },
-                    new Specialist { Id = Guid.NewGuid(), FullName = "Andrew" }
+                    new Specialist { Id = Guid.NewGuid(), FullName = "Vanya", Comment = "worker" },
+                    new Specialist { Id = Guid.NewGuid(), FullName = "Andrew", Comment = "best worker" }
                 });
                 Specialists.AddRange(specialists);
 
@@ -63,7 +66,13 @@ namespace Workshop.Data
                     new Device { Id = Guid.NewGuid(), Brand = "Xiaomi", Model = "Redmi 9", Type = DeviceType.Phone },
                     new Device { Id = Guid.NewGuid(), Brand = "Huawei", Model = "P Smart Plus", Type = DeviceType.Phone },
                     new Device { Id = Guid.NewGuid(), Brand = "Samsung", Model = "S23", Type = DeviceType.Phone },
-                    new Device { Id = Guid.NewGuid(), Brand = "Pixel", Model = "8", Type = DeviceType.Phone }
+                    new Device { Id = Guid.NewGuid(), Brand = "Pixel", Model = "8", Type = DeviceType.Phone },
+                    new Device { Id = Guid.NewGuid(), Brand = "Huawei", Model = "P Smart Z", Type = DeviceType.Phone },
+                    new Device { Id = Guid.NewGuid(), Brand = "Samsung", Model = "A515", Type = DeviceType.Phone },
+                    new Device { Id = Guid.NewGuid(), Brand = "Pixel", Model = "6", Type = DeviceType.Phone },
+                    new Device { Id = Guid.NewGuid(), Brand = "Xiaomi", Model = "Redmi Note 7", Type = DeviceType.Phone },
+                    new Device { Id = Guid.NewGuid(), Brand = "Samsung", Model = "A325", Type = DeviceType.Phone },
+                    new Device { Id = Guid.NewGuid(), Brand = "Apple", Model = "Iphone 11 Pro", Type = DeviceType.Phone }
                 });
                 Devices.AddRange(devices);
 
@@ -91,48 +100,44 @@ namespace Workshop.Data
                     new StockItem
                     {
                         Item = new Item
-                        {
-                            Id = Guid.NewGuid(),
-                            Type = ItemType.LCD,
-                            Title = "Pixel 8 LCD",
-                            Device = new Device { Id = Guid.Parse("2e5b3fb8-9839-42ba-87ea-441e9680b8f7") }
-                        },
-                        Price = 8500,
-                        Quantity = 2
+                        { Id = Guid.NewGuid(), Type = ItemType.LCD, Title = "Pixel 8 LCD", Device = devices.First(d => d.Brand.Contains("Pixel") && d.Model.Contains("8"))},
+                        Price = 8500, Quantity = 2
                     },
                     new StockItem
                     {
                         Item = new Item
-                        {
-                            Id = Guid.NewGuid(),
-                            Type = ItemType.Battery,
-                            Title = "Samsung A515 Battery",
-                            Device = new Device
-                            {
-                                Brand = "Samsung",
-                                Model = "A515",
-                                Type = DeviceType.Phone
-                            }
-                        },
-                        Price = 400,
-                        Quantity = 12
+                        { Id = Guid.NewGuid(), Type = ItemType.Battery, Title = "Samsung A515 Battery", Device = devices.First(d => d.Brand.Contains("Samsung") && d.Model.Contains("A515"))},
+                        Price = 400, Quantity = 12
                     },
                     new StockItem
                     {
                         Item = new Item
-                        {
-                            Id = Guid.NewGuid(),
-                            Type = ItemType.BackCover,
-                            Title = "Xiaomi Redmi Note 8 Back Cover",
-                            Device = new Device
-                            {
-                                Brand = "Xiaomi",
-                                Model = "Redmi Note ",
-                                Type = DeviceType.Phone
-                            }
-                        },
-                        Price = 250,
-                        Quantity = 3
+                        { Id = Guid.NewGuid(), Type = ItemType.LCD, Title = "Samsung A515 LCD", Device = devices.First(d => d.Brand.Contains("Samsung") && d.Model.Contains("A515"))},
+                        Price = 1200, Quantity = 7
+                    },
+                    new StockItem
+                    {
+                        Item = new Item
+                        { Id = Guid.NewGuid(), Type = ItemType.BackCover, Title = "Xiaomi Redmi 9 Back Cover", Device = devices.First(d => d.Brand.Contains("Xiaomi") && d.Model.Contains("Redmi 9"))},
+                        Price = 250, Quantity = 3
+                    },
+                    new StockItem
+                    {
+                        Item = new Item
+                        { Id = Guid.NewGuid(), Type = ItemType.Battery, Title = "Xiaomi Redmi 9 Battery", Device = devices.First(d => d.Brand.Contains("Xiaomi") && d.Model.Contains("Redmi 9"))},
+                        Price = 400, Quantity = 2
+                    },
+                    new StockItem
+                    {
+                        Item = new Item
+                        { Id = Guid.NewGuid(), Type = ItemType.LCD, Title = "Xiaomi Redmi 9 LCD", Device = devices.First(d => d.Brand.Contains("Xiaomi") && d.Model.Contains("Redmi 9"))},
+                        Price = 870, Quantity = 2
+                    },
+                    new StockItem
+                    {
+                        Item = new Item
+                        { Id = Guid.NewGuid(), Type = ItemType.Battery, Title = "Pixel 8 Battery", Device = devices.First(d => d.Brand.Contains("Pixel") && d.Model.Contains("8"))},
+                        Price = 500, Quantity = 5
                     }
                 });
                 Stock.AddRange(stockItems);
@@ -149,9 +154,48 @@ namespace Workshop.Data
                         Device = devices[0],
                         Complaint = "device not booting",
                         Status = RepairStatus.Accepted
+                    },
+                    new Repair
+                    {
+                        Specialist = specialists[0],
+                        Client = clients[1],
+                        Device = devices[1],
+                        Complaint = "device not booting",
+                        Status = RepairStatus.Accepted
+                    },
+                    new Repair
+                    {
+                        Specialist = specialists[0],
+                        Client = clients[2],
+                        Device = devices[2],
+                        Complaint = "device not booting",
+                        Status = RepairStatus.Accepted
+                    },
+                    new Repair
+                    {
+                        Specialist = specialists[1],
+                        Client = clients[3],
+                        Device = devices[3],
+                        Complaint = "device not booting",
+                        Status = RepairStatus.Accepted
+                    },
+                    new Repair
+                    {
+                        Specialist = specialists[1],
+                        Client = clients[4],
+                        Device = devices[4],
+                        Complaint = "device not booting",
+                        Status = RepairStatus.Accepted
+                    },
+                    new Repair
+                    {
+                        Specialist = specialists[1],
+                        Client = clients[3],
+                        Device = devices[2],
+                        Complaint = "device not booting",
+                        Status = RepairStatus.Accepted
                     }
                 );
-
             }
             SaveChanges();
         }
